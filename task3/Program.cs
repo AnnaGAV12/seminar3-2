ddc9bc2b37c85beaec01de68351a2a3fd9ebecf5
Anna <ds_anna@bk.ru>
2022-05-18 22:14:30 +0300

    Найти количество чисел

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти количество чисел
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int index = 1;
+while (number > 9)
+{
+    number = number / 10;
+    index++;
+}
+Console.WriteLine($"Количество чисел {index}");
+
+//Вариант 2 через строку-посчитать длину строки
+//int number = int.Parse(Console.ReadLine());
+//string c;
+//c = number.Tostring();
+//int c1 = c.Length;
+//Console.WriteLine($"Количество чисел в числе {number} - {c1}");
