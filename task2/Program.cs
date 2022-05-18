90e548e0f5248a2d260f3b9b1d3ee1b621177a51
Anna <ds_anna@bk.ru>
2022-05-18 22:13:55 +0300

    Найти сумму чисел от 1 до А

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+//int sum = 0;
+//for (int i = 1; i <= a; i++)
+//{
+//    sum = sum + i;
+//}
+
+// Через арифметическую прогрессию
+int sum=a*(a+1)/2;
+Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
